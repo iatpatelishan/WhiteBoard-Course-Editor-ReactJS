a300c38137d16983d94f33a5c2329bf0f408137e
Ishan Patel <i@patelishan.com>
2018-05-22 01:51:54 -0400

    Deleted Default React files and Added new Index.js

---
+++ src/containers/CourseManager.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import CourseManager from './containers/CourseManager';
+
+class CourseManager extends Component {
+    render() {
+        return (
+            <h1>Course Manager</h1>
+        )
+    }
+}
+export default CourseManager;
