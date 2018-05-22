4e66fd8491eb7eb54d97e20ab46a4d1ea9043281
Ishan Patel <i@patelishan.com>
2018-05-22 05:03:10 -0400

    CSS style change to CourseManager

--- src/containers/CourseManager.js
+++ src/containers/CourseManager.js
@@ -1,14 +1,45 @@
 import React, {Component} from 'react';
+import './CourseManager.css';
+import $ from "jquery";
+import swal from 'sweetalert';
 import CourseList from './CourseList'
+
+
+const navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
+
+            <div className="form input-group input-group-lg">
+                <a className="navbar-brand" href="#"><h4>Course Manager</h4></a>
+                <input className="form-control add-course-input" type="text" placeholder="New Course Title"/>
+                <div class="wbdv-top-create-outer">
+                    <div class="wbdv-top-create-inner">
+                    </div>
+                    <i className="fa fa-3x fa-plus-circle wbdv-create-btn" onClick={() => {
+                        swal("Hello world!")
+                    }}></i>
+                </div>
+            </div>
+        </nav>
+    );
+}
 
 class CourseManager extends Component {
     render() {
         return (
             <div>
-                <h1>Course Manager</h1>
-                <CourseList/>
+                {navbar()}
+                <br/><br/><br/><br/>
+                <div className="container">
+                    <CourseList/>
+                </div>
+
+                <i class="btn-fab fa fa-3x fa-plus-circle wbdv-create-btn" onClick={() => {
+                    swal("Hello world!")
+                }}></i>
             </div>
         )
     }
 }
+
 export default CourseManager;
