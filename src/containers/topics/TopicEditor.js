347b697e0152e827d81ed587827d9a55f90b859a
Ishan Patel <i@patelishan.com>
2018-05-23 19:42:02 -0400

    Created Topic Editor

---
+++ src/containers/topics/TopicEditor.js
@@ -0,0 +1,52 @@
+import React, {Component} from "react";
+
+export default class TopicEditor extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {courseId: '', moduleId: '', lessonId:'', topicId: ''};
+        this.setCourseId = this.setCourseId.bind(this);
+        this.setModuleId = this.setModuleId.bind(this);
+        this.setLessonId = this.setLessonId.bind(this);
+        this.setTopicId = this.setTopicId.bind(this);
+    }
+
+    componentDidMount() {
+        this.setCourseId(this.props.match.params.courseId);
+        this.setModuleId(this.props.match.params.moduleId);
+        this.setLessonId(this.props.match.params.lessonId);
+        this.setTopicId(this.props.match.params.topicId);
+    }
+
+    componentWillReceiveProps(newProps) {
+        this.setCourseId(newProps.match.params.courseId);
+        this.setModuleId(newProps.match.params.moduleId);
+        this.setLessonId(newProps.match.params.lessonId);
+        this.setTopicId(newProps.match.params.topicId);
+    }
+
+    setCourseId(courseId) {
+        this.setState
+        ({courseId: courseId});
+    }
+
+    setModuleId(moduleId) {
+        this.setState({moduleId: moduleId});
+    }
+
+    setLessonId(lessonId) {
+        this.setState({lessonId: lessonId});
+    }
+
+    setTopicId(topicId) {
+        this.setState({topicId: topicId});
+    }
+
+
+    render(){
+        return(
+            <div className="container">
+                {this.state.topicId} TOPIC HERE
+            </div>
+        )
+    }
+}
