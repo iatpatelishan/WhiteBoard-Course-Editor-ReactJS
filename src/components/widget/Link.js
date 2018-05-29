fd53fd4eb44e501b599f482563421fafba17c2c1
Ishan Patel <i@patelishan.com>
2018-05-29 01:32:38 -0400

    Created Link componenet

---
+++ src/components/widget/Link.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+import {connect} from "react-redux";
+import * as actions from "../../actions/index"
+
+const Link = () => (
+    <div>
+        <h2>Link</h2>
+    </div>
+)
+
+export default Link;
