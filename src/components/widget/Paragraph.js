e2a2127ef333e9f3ca6aa54a97bc00fc53783518
Ishan Patel <i@patelishan.com>
2018-05-29 01:33:02 -0400

    Created Paragraph component

---
+++ src/components/widget/Paragraph.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import {connect} from "react-redux";
+import * as actions from "../../actions/index"
+
+const Paragraph = () => (
+    <div>
+        <h2>Paragraph</h2>
+        <textarea></textarea>
+    </div>
+)
+
+export default Paragraph;
