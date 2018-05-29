3bcb43e3d534c6a3eaac6e76cd11f25342d4900a
Ishan Patel <i@patelishan.com>
2018-05-29 01:32:46 -0400

    Created List componenet

---
+++ src/components/widget/List.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+import {connect} from "react-redux";
+import * as actions from "../../actions/index"
+
+const List = () => (
+    <div>
+        <h2>Image</h2>
+    </div>
+)
+
+export default List;
